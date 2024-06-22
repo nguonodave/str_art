9e723ab8d0803e8a053894a1146a3ed962f88a20
David Ochiel <nguonodave@gmail.com>
2024-06-23 02:31:58 +0300

    feat: enforce good practices on file integrity feature

--- read_files/read_files.go
+++ read_files/read_files.go
@@ -10,41 +10,37 @@ import (
 )
 
 func ReadFiles(bytes []byte, read_err error, lines []string) ([]string, bool) {
-	std_chmod_error := os.Chmod("banners/standard.txt", 0o400)
-	shdw_chmod_error := os.Chmod("banners/shadow.txt", 0o400)
-	thnky_chmod_error := os.Chmod("banners/thinkertoy.txt", 0o400)
-
-	if std_chmod_error != nil {
-		log.Fatal(std_chmod_error)
-	} else if shdw_chmod_error != nil {
-		log.Fatal(shdw_chmod_error)
-	} else if thnky_chmod_error != nil {
-		log.Fatal(thnky_chmod_error)
-	}
-
-	var file_altered bool
+	var file_path string
+	var original_hash string
 
 	if len(os.Args) == 2 || (len(os.Args) == 3 && os.Args[2] == "standard") {
-		bytes, read_err = os.ReadFile("banners/standard.txt")
-		file_altered = file_integrity.FileAltered(bytes, "e194f1033442617ab8a78e1ca63a2061f5cc07a3f05ac226ed32eb9dfd22a6bf")
+		file_path = "banners/standard.txt"
+		original_hash = "e194f1033442617ab8a78e1ca63a2061f5cc07a3f05ac226ed32eb9dfd22a6bf"
 	} else if os.Args[2] == "shadow" {
-		bytes, read_err = os.ReadFile("banners/shadow.txt")
-		file_altered = file_integrity.FileAltered(bytes, "26b94d0b134b77e9fd23e0360bfd81740f80fb7f6541d1d8c5d85e73ee550f73")
+		file_path = "banners/shadow.txt"
+		original_hash = "26b94d0b134b77e9fd23e0360bfd81740f80fb7f6541d1d8c5d85e73ee550f73"
 	} else if os.Args[2] == "thinkertoy" {
-		bytes, read_err = os.ReadFile("banners/thinkertoy.txt")
-		file_altered = file_integrity.FileAltered(bytes, "64285e4960d199f4819323c4dc6319ba34f1f0dd9da14d07111345f5d76c3fa3")
+		file_path = "banners/thinkertoy.txt"
+		original_hash = "64285e4960d199f4819323c4dc6319ba34f1f0dd9da14d07111345f5d76c3fa3"
 	} else {
 		fmt.Println("Usage: go run . [STRING] [BANNER]\n\nEX: go run . something standard")
 		return nil, true
 	}
 
+	if chmod_err := os.Chmod(file_path, 0o400); chmod_err != nil {
+		log.Fatal(chmod_err)
+	}
+
+	bytes, read_err = os.ReadFile(file_path)
 	if read_err != nil {
 		fmt.Println(read_err)
 	}
 
+	file_altered := file_integrity.FileAltered(bytes, original_hash)
+
 	// print error message if a file is altered
 	if file_altered {
-		fmt.Println("Seems like you altered the data file(s). Please download them from here")
+		fmt.Println("Seems like you altered the data file(s). Please download them from the following link\n\nhttps://learn.zone01kisumu.ke/git/root/public/src/branch/master/subjects/ascii-art")
 		return nil, true
 	}
 
